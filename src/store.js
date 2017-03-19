3430ea75f074651c3b2084c62e9431d7915a84d3
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2017-03-18 23:34:05 -0500

    Implements combine reducers

--- src/store.js
+++ src/store.js
@@ -1,27 +1,25 @@
-import { createStore, applyMiddleware }  from 'redux'
+import { createStore, applyMiddleware, combineReducers }  from 'redux'
 import thunk from 'redux-thunk'
 
-const reducer = (state, action) => {
-  if(action.type === "LOAD_PRODUCTS") {
-    return {
-      ...state,
-      products: action.products
-    };
-  }
-  else if(action.type === "ADD_TO_CART") {
-    return {
-      ...state,
-      cart: state.cart.concat(action.product)
-    };
+const cart = (state=[], action) => {
+  if(action.type === "ADD_TO_CART") {
+    return state.concat(action.product)
   } else if(action.type === "REMOVE_FROM_CART") {
-    return {
-      ...state,
-      cart: state.cart.filter(product => product.id !== action.product.id)
-    };
+    return state.filter(product => product.id !== action.product.id)
   }
 
   return state;
 };
+
+const products = (state=[], action) => {
+  if(action.type === "LOAD_PRODUCTS") {
+    return action.products;
+  }
+
+  return state;
+};
+
+
 
 const logger = store => next => action => {
   console.log('dispatching', action);
@@ -30,4 +28,4 @@ const logger = store => next => action => {
   return result;
 }
 
-export default createStore(reducer, { cart: [], products: [] }, applyMiddleware(logger, thunk))
+export default createStore(combineReducers({ cart, products }), applyMiddleware(logger, thunk))
